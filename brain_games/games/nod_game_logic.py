08d1a7c5248718f828b312107ae956a48b073e76
Сергей Данилов <ushankax@ushankax.local>
2020-01-05 14:52:22 +0300

    made brain-gcd game

---
+++ brain_games/games/nod_game_logic.py
@@ -0,0 +1,31 @@
+import prompt
+import random
+
+def welcome_nod():
+    print('Welcome to the Brain Games!')
+    print('Find the greatest common divisor of given numbers.')
+    print('')
+
+
+def greatest_div():
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+    i = min(num1, num2)
+    question = ('Question: {} {}'.format(num1, num2))
+    print(question)
+
+    while i > 0:
+        if num1 % i == 0 and num2 % i == 0:
+            return i
+            break
+        else:
+            i -= 1
+
+
+def answering():
+    result = greatest_div()
+    guess = prompt.string('Your answer: ')
+    return str(result) == str(guess)
+
+
+
